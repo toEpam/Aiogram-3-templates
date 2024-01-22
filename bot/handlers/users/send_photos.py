d52a431987309bf7cacd5e2ad716280259a4e314
toEpam <toepammiddle@gmail.com>
2024-01-22 16:37:37 +0500

    Midia is going to be avilable in template soon.

--- bot/handlers/users/send_photos.py
+++ bot/handlers/users/send_photos.py
@@ -20,34 +20,26 @@ async def get_file_id_v(message: Message):
 
 @media_router.message(Command("kitob"))
 async def send_book(message: Message):
-    photo_id = "AgACAgUAAxkBAAIHUWErOgOL_YUiW1bawxdvEJM8mUd9AAK4rDEbXltZVRPBqDf39UdmAQADAgADeQADIAQ"
-    photo_url = "https://kitoblardunyosi.uz/image/cache/catalog/001-Kitoblar/003_boshqalar/006_ilmiy_ommabop/python-3d-web-500x500h.jpg"
-    # photo_file = InputFile(path_or_bytesio="/media/images/kitob.jpg")
+    photo_url = "https://telegra.ph/file/fd053ddb8d0fbee95e85e.png"
     photo_file = FSInputFile(path="./media/images/kitob.jpg")
-    await message.reply_photo(
-        photo_file, caption="Dasturlash asoslari kitobi. \n Narxi: 50000 so'm"
-    )
-    await message.answer_photo(
-        photo_id, caption="Dasturlash asoslari kitobi. \n Narxi: 50000 so'm"
-    )
     await bot.send_photo(
         chat_id=message.from_user.id,
         photo=photo_url,
         caption="Dasturlash asoslari kitobi. \nNarxi: 50000 so'm",
+    )
+    await message.reply_photo(
+        photo_file, caption="Dasturlash asoslari kitobi. \n Narxi: 50000 so'm"
     )
 
 
 @media_router.message(Command("kurslar"))
 async def send_courses(message: Message):
     media_group = MediaGroupBuilder()
-    photo1 = "AgACAgUAAxkBAAIHa2ErPWwh50geLqoE4Hn5SGCdi09mAAK8rDEbXltZVUoM_yKC7xVRAQADAgADeQADIAQ"
-    photo2 = "https://i0.wp.com/mohirdev.uz/wp-content/uploads/photo1627374915.jpeg"
-    photo3 = "https://i1.wp.com/mohirdev.uz/wp-content/uploads/Telegram-bot.png"
-    photo4 = InputFile(filename="/media/images/algoritm.png")
-    video1 = "BAACAgUAAxkBAAIHT2ErOXldS7NxbW9mdL4tsI18ZqlvAALdAgACXltZVVoMJafxpb77IAQ"
+    photo1 = "https://i1.wp.com/mohirdev.uz/wp-content/uploads/Telegram-bot.png"
+    photo2 = InputFile(filename="./media/images/algoritm.png")
+    photo3 = "AgACAgIAAxkBAAEYSIRlrlKZuq62fZDh5mc8w9oBP174egACSs8xG5FfeUkfo12lWIhSEAEAAwIAA3gAAzQE"
+    video = "BQACAgIAAxkBAAEYSIllrlM_qgGdxLG8MGK5O9BbQ_DgUgAC4jwAApFfeUnQMpGuXu2X-DQE"
     media_group.add_photo(photo1)
     media_group.add_photo(photo2)
-    media_group.add_photo(photo3)
-    media_group.add_photo(photo4)
-    media_group.add_video(video=video1, caption="Bizning online kurslarimiz")
+    media_group.add_video(video=video, caption="Bizning online kurslarimiz")
     await message.reply_media_group(media_group)
