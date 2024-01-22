77e7bfd852d8f72aa36d6a44dc4dead5235bcaa8
toEpam <toepammiddle@gmail.com>
2024-01-22 09:06:34 +0500

    Default commands creation made easy

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -3,15 +3,15 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 
 
 async def set_commands(bot: Bot):
+    commands_list = [
+        ["start", "Botni ishga tushurish"],
+        ["help", "Yordam"],
+    ]
     commands = [
         BotCommand(
-            command="start",
-            description="Botni ishga tushurish",
-        ),
-        BotCommand(
-            command="help",
-            description="Yordam",
-        ),
+            command=command[0],
+            description=command[1] if len(command) == 2 else command[0],
+        ) for command in commands_list
     ]
 
     await bot.set_my_commands(commands=commands, scope=BotCommandScopeDefault())
